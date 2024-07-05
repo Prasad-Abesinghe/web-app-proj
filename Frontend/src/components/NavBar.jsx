e3b4b9765c8e4a3542ef360a7a151a94a7613235
Prasad-Abesinghe <lahiruprasad379@gmail.com>
2024-07-05 13:38:53 +0530

    add side bar component

--- Frontend/src/components/NavBar.jsx
+++ Frontend/src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { FaMobileAlt } from "react-icons/fa";
 import { FaBarsStaggered, FaCartShopping, FaXmark } from "react-icons/fa6";
@@ -6,8 +6,15 @@ import { CgProfile } from "react-icons/cg";
 import { AuthContext } from "../contects/AuthProvider";
 import "../index.css";
 import { Button } from "flowbite-react";
+//for side bar
+("use client");
+import { Drawer } from "flowbite-react";
 
 const NavBar = () => {
+  //for sidebar
+  const [isOpen, setIsOpen] = useState(true);
+  const handleClose = () => setIsOpen(false);
+
   const [isSticky, setSticky] = useState(false);
 
   const { user } = useContext(AuthContext);
@@ -34,7 +41,6 @@ const NavBar = () => {
     { link: "DashBoard", path: "/admin/dashboard" },
   ];
 
-
   return (
     <header className=" bg-white w-full bg-transparent fixed top-0 left-0 right-0 transition-all ease-in duration-300">
       <nav
@@ -56,29 +62,54 @@ const NavBar = () => {
                   {link}
                 </Link>
               ))}
-              <Link>
-                <FaCartShopping className="text-[26px]" />
-              </Link>
+              <div>
+                <div className=" relative cursor-pointer">
+                  <button onClick={() => setIsOpen(true)}>
+                    <FaCartShopping className="text-[26px]" />
+                  </button>
 
-
-              {user? (<Link>
-                <div className="">
-                  <img
-                    src={user?.photoURL}
-                   // alt={<CgProfile />}
-                    defaultValue={<CgProfile />}
-                    className="rounded-full"
-                    width="30px"
-                  />
+                  <div className="bg-accent w-[18px] h-[18px] absolute -right-1 -bottom-1 rounded-full text-white flex items-center justify-center text-sm font-medium">
+                    0
+                  </div>
                 </div>
+                <div>
+                  <Drawer open={isOpen} onClose={handleClose} position="right">
+                    <Drawer.Header
+                      title="My Shopping Cart"
+                    />
+                    <Drawer.Items></Drawer.Items>
+                  </Drawer>
+                </div>
+              </div>
 
-                {/* <  CgProfile className="text-[26px]" /> */}
-              </Link>):(
-                <Link to="sign-up">                <Button className={` btn-primary border-none ${
-                  isSticky ? "sticky btn-accent bg-[#D82349] hover:bg-[#B32241] border-none" : ""
-                }`}>Sign In</Button>
+              {user ? (
+                <Link>
+                  <div className="">
+                    <img
+                      src={user?.photoURL}
+                      // alt={<CgProfile />}
+                      defaultValue={<CgProfile />}
+                      className="rounded-full"
+                      width="30px"
+                    />
+                  </div>
+
+                  {/* <  CgProfile className="text-[26px]" /> */}
                 </Link>
-              )}        
+              ) : (
+                <Link to="sign-up">
+                  {" "}
+                  <Button
+                    className={` btn-primary border-none ${
+                      isSticky
+                        ? "sticky btn-accent bg-[#D82349] hover:bg-[#B32241] border-none"
+                        : ""
+                    }`}
+                  >
+                    Sign In
+                  </Button>
+                </Link>
+              )}
             </ul>
           </div>
         </div>
--- Frontend/src/shop/Shop.jsx
+++ Frontend/src/shop/Shop.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Card } from "flowbite-react";
+import { Button, Card } from "flowbite-react";
 import { Link } from "react-router-dom";
+import ShopSideBar from "./ShopSideBar";
 
 const Shop = () => {
   const [phones, setPhones] = useState([]);
@@ -11,41 +12,39 @@ const Shop = () => {
       .then((data) => setPhones(data));
   }, []);
   return (
-    <div className=" mt-18 py-20 px-4 lg:px-24 bg-slate-300">
-      <h2 className=" text-5xl font-bold text-center">All Phones are here</h2>
-      <div className=" grid gap-8 my-12 lg:grid-cols-5 sm:grid-cols-2 md:grid-cols-3 grid-cols-1 ">
-        {phones.map((phone) => (
-          <Card className="max-w-sm ">
-            <div className="bg-slate-600 m-2 rounded-md">
-              <div className=" flex flex-col gap-2 justify-center items-center">
-                <img src={phone.imageUrl} alt="" className=" w-full" />
+    <div className="flex">
+      <ShopSideBar/>
+      <div className=" mt-18 py-20 px-4 lg:px-24 bg-slate-300">
+        <h2 className=" text-5xl font-bold text-center">All Phones are here</h2>
+        <div className=" grid gap-8 my-12 lg:grid-cols-5 sm:grid-cols-2 md:grid-cols-3 grid-cols-1 ">
+          {phones.map((phone) => (
+            <Card className="max-w-md items-center ">
+              <div className="bg-slate-600 w-full rounded-md">
+                <div className=" flex flex-col gap-2 justify-center items-center">
+                  <img src={phone.imageUrl} alt="" className=" w-full" />
 
-                <h2>{phone.mobileName}</h2>
-                <a href="#">
-                  <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                    {phone.brand}
-                  </h5>
-                </a>
-                <span className="text-3xl font-bold text-gray-900 dark:text-white">
-                  ${phone.price}
-                </span>
-              </div>
-              <div className="flex items-center justify-between">
-                <Link to={`/phone/${phone._id}`}>
-                  <a className="rounded-lg btn-accent px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800">
-                    More About
+                  <h2>{phone.mobileName}</h2>
+                  <a href="#">
+                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                      {phone.brand}
+                    </h5>
                   </a>
-                </Link>
-                <a
-                  href="#"
-                  className="rounded-lg btn-primary px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
-                >
+                  <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                    ${phone.price}
+                  </span>
+                </div>
+                <button className=" w-full mb-2 btn-primary px-4.5 py-2 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800">
                   Add to cart
-                </a>
+                </button>
+                <Link to={`/phone/${phone._id}`}>
+                  <button className=" btn-accent w-full px-5 py-2 border-none text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800">
+                    More About
+                  </button>
+                </Link>
               </div>
-            </div>
-          </Card>
-        ))}
+            </Card>
+          ))}
+        </div>
       </div>
     </div>
   );
