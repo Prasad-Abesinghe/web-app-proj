e61fab60394c18a32fa24431fa33f4763f1e4520
Prasad-Abesinghe <lahiruprasad379@gmail.com>
2024-07-22 16:05:41 +0530

    add some syles for shop

--- Frontend/src/shop/Shop.jsx
+++ Frontend/src/shop/Shop.jsx
@@ -27,30 +27,30 @@ const Shop = () => {
   }, []);
   return (
     
-    <div className=" ml-60 mt-18 py-20 mx-0 lg:px-10 md:px-6 bg-slate-300">
-      <h2 className=" text-5xl font-bold text-center">All Phones are here</h2>
+    <div className=" ml-60 mt-18 py-20 mx-0 lg:px-10 md:px-6">
+      <h2 className=" text-5xl font-bold text-center">All Phones <span className=" bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">are here</span> </h2>
       <div className=" grid gap-8 my-12 lg:grid-cols-5 sm:grid-cols-2 md:grid-cols-2 grid-cols-1 ">
         {phones.map((phone) => (
-          <Card className="max-w-md items-center hover:scale-110 ">
-            <div className="bg-slate-600 w-full rounded-md">
+          <Card className="max-w-md items-center border-none hover:scale-110 ">
+            <div className=" w-full bg-gradient-to-b from-gray-600 via-black to-gray-700 p-4 rounded-md">
               <div className=" flex flex-col gap-2 justify-center items-center">
-                <img src={phone.imageUrl} alt="" className=" w-full" />
+                <img src={phone.imageUrl} alt="" className=" w-full rounded-md" />
 
                 <h2>{phone.mobileName}</h2>
                 <a href="#">
-                  <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                  <h5 className="text-xl font-semibold tracking-tight dark:text-white">
                     {phone.brand}
                   </h5>
                 </a>
-                <span className="text-3xl font-bold text-gray-900 dark:text-white">
+                <span className="text-3xl font-bold dark:text-white">
                   ${phone.price}
                 </span>
               </div>
-              <button className=" w-full mb-2 btn-primary px-4.5 py-2 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800" onClick={()=>{addToCart(phone)}}>
+              <button className=" w-full mb-2 rounded-md btn-accent px-4.5 py-2 text-center text-sm font-medium text-white" onClick={()=>{addToCart(phone)}}>
                 Add to cart
               </button>
               <Link to={`/phone/${phone._id}`}>
-                <button className=" btn-accent w-full px-5 py-2 border-none text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800">
+                <button className="  btn-primary rounded-md w-full px-5  py-2 text-center text-sm font-medium text-white ">
                   More About
                 </button>
               </Link>
